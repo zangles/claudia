294df53df1ecaff5b02257956532b4e661ad3af6
zangles <azuresky07@gmail.com>
2017-08-20 19:44:04 -0300

    NUTRILOG-4 correcciones varias

--- resources/views/patients/partials/peso.blade.php
+++ resources/views/patients/partials/peso.blade.php
@@ -1,6 +1,11 @@
 <div class="ibox float-e-margins">
     <div class="ibox-title">
         <h5>Grafico evolucion peso</h5>
+        <div class="ibox-tools">
+            <a class="collapse-link">
+                <i class="fa fa-chevron-up"></i>
+            </a>
+        </div>
     </div>
     <div class="ibox-content">
         <div style="width:100%;">
