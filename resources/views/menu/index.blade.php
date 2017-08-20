0e4dfa8ee980450d58de0928577332630cefbef3
zangles <azuresky07@gmail.com>
2017-08-20 20:05:13 -0300

    cambio title login

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -4,8 +4,8 @@
             <li class="nav-header">
                 @include('menu.partials.profile')
             </li>
-            <li>
-                <a href="#"><i class="fa fa-shield"></i> <span class="nav-label">Dashboard</span></a>
+            <li class="{{ Request::is('home') ? 'active' : '' }}">
+                <a href="{{ route('home') }}"><i class="fa fa-shield"></i> <span class="nav-label">Dashboard</span></a>
             </li>
             <li class="{{ Request::is('patient') ? 'active' : '' }}">
                 <a href="{{ route('patient.index')}}"><i class="fa fa-users"></i> <span class="nav-label">Pacientes</span></a>
